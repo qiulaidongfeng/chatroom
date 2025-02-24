baede8f9494e5e6f1e4cb71594b8d2d10028b4ec
qiulaidongfeng <2645477756@qq.com>
2025-02-24 23:28:54 +0800

    添加测试执行用时脚本

---
+++ main_for_test.py
@@ -0,0 +1,36 @@
+from channel.channel import *
+import time
+
+
+def main():
+    start = time.time()
+    c = Channel()
+    print(time.time() - start)
+
+    start = time.time()
+    c.CreateRoom("test")
+    print(time.time() - start)
+
+    start = time.time()
+    c.SendMessage("test", "k")
+    print(time.time() - start)
+
+    start = time.time()
+    c.waitMessage()
+    print(time.time() - start)
+
+    start = time.time()
+    msg = c.EnterRoom("test")
+    print(time.time() - start)
+
+    start = time.time()
+    c.ExitRoom("test")
+    print(time.time() - start)
+
+    assert msg != None
+    assert msg[0] == "k"
+    return
+
+
+if __name__ == "__main__":
+    main()
